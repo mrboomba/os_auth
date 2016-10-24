93467bd624763c5f9f2a65b44d81bc280544b2e9
Mr.Boomba <mrboomba161@gmail.com>
2016-10-24 16:22:39 +0700

    second commit

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '217970568615796', // your App ID
         'clientSecret'  : 'a2a084de8443964c293db0e032242f58', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://ec2-35-161-11-183.us-west-2.compute.amazonaws.com:/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'tLiadESpmDXfyDM8Y0sbcgDZu',
         'consumerSecret'    : 'hyIlywCVWc9RdEZBZ1tIDDE1v3SbLelWqvx6kSXN282QorZFUr',
-        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://ec2-35-161-11-183.us-west-2.compute.amazonaws.com:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '506545026994-29hd1lvqlt9uhidisido3b8v04217umm.apps.googleusercontent.com',
         'clientSecret'  : 'm1zZMoM7cs3_wu8RhCeSDJWf',
-        'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
+        'callbackURL'   : 'http://ec2-35-161-11-183.us-west-2.compute.amazonaws.com:8080/auth/google/callback'
     }
 
 };
